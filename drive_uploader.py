026826f66fff984156a648e5fd1d116516af75aa
ImmortalNail <v.alex.sky@gmail.com>
2025-05-25 03:38:28 +0300

    Add files via upload

---
+++ drive_uploader.py
@@ -0,0 +1,37 @@
+import os
+import json
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from google.oauth2.service_account import Credentials
+
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+
+def upload_file_to_drive(file_path: str, folder_id: str, file_name: str = None) -> str:
+    # Чтение JSON-ключа из переменной окружения
+    json_str = os.environ.get("GOOGLE_SERVICE_ACCOUNT_JSON")
+    if not json_str:
+        raise Exception("Переменная окружения GOOGLE_SERVICE_ACCOUNT_JSON не задана")
+
+    credentials_info = json.loads(json_str)
+    creds = Credentials.from_service_account_info(credentials_info, scopes=SCOPES)
+    service = build('drive', 'v3', credentials=creds)
+
+    if file_name is None:
+        file_name = os.path.basename(file_path)
+
+    file_metadata = {
+        'name': file_name,
+        'parents': [folder_id]
+    }
+    media = MediaFileUpload(file_path, resumable=True)
+    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    file_id = file.get('id')
+
+    # Делаем файл доступным по ссылке
+    permission = {
+        'type': 'anyone',
+        'role': 'reader',
+    }
+    service.permissions().create(fileId=file_id, body=permission).execute()
+
+    return f'https://drive.google.com/uc?id={file_id}'
